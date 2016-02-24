2c4774b698e7cc307476688a08fdb01cfd458050
whereisirene <yl53@uw.edu>
2016-02-23 22:51:56 -0800

    Done by Irene

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # INFO-498F-a7-anythinginfo
+
 data <- read.csv("https://raw.githubusercontent.com/INFO-498F/a7-survey-data/master/intro_survey_data.csv")
 
--- scripts/summary.R
+++ scripts/summary.R
@@ -1,55 +1,26 @@
 # Function that takes in data from the class survey and returns a list of information
 
 library(dplyr)
-info_data <- read.csv("https://raw.githubusercontent.com/INFO-498F/a7-survey-data/master/intro_survey_data.csv", stringsAsFactors = FALSE)
 
 info_summary <- function(dataset) {
-  # Renaming column names
-  new_info <- rename(info_data, 
-                     standing = What.is.your.current.class.standing., 
-                     informatics = Are.you.interested.in.applying.to.the.Informatics.major.,
-                     os = What.operating.system.do.you.typically.use.,
-                     rprogram = What.is.your.familiarity.with..Using.the.R.programming.language,
-                     travel = How.many.countries.have.you.visited.in.your.life.,
-                     pets = Do.you.consider.yourself.
-                     )
+  
   # List of information
-  #info_list <- list()
+  info_list <- list()
   
-  # Calculating number of students in each class standing
-    class_standing <- new_info %>%
-      group_by(standing) %>%
-      summarise(
-        standing_count = length(standing)) %>%
-      select(standing_count)
+  # Calculate the number of students in class
+  info_list$num <- nrow(dataset)
   
-  # Number of people interested in the Informatics program
-    interested <- new_info %>%
-      group_by(informatics) %>%
-      summarise(
-        interest_count = length(informatics)
-      ) %>%
-      select(interest_count)
+  # Calculate number of students in each class standing
+  info_list$numFreshmen <- nrow(filter(dataset, What.is.your.current.class.standing. == "Freshman"))
+  info_list$numSophomore <- nrow(filter(dataset, What.is.your.current.class.standing. == "Sophomore"))
+  info_list$numJunior <- nrow(filter(dataset, What.is.your.current.class.standing. == "Junior"))
+  info_list$numSenior <- nrow(filter(dataset, What.is.your.current.class.standing. == "Senior"))
+  
+  # Calculate the number of Windows and Mac users
+  info_list$numWindows <- nrow(filter(dataset, What.operating.system.do.you.typically.use. == "Windows"))
+  info_list$numMac <- nrow(filter(dataset, What.operating.system.do.you.typically.use. == "Mac"))
     
-  # What type of operating system students used
-    operating <- new_info %>%
-      group_by(os) %>%
-      summarise(
-        os_count = length(os)
-      ) %>%
-      select(os_count)
-    
-  # Familiarity with R programming language
-    rlanguage <- new_info %>%
-      group_by(rprogram) %>%
-      summarise(
-        r_count = length(rprogram)
-      ) %>%
-      select(r_count)
-    
-  # How many countries has students traveled to
-    countries <- filter(new_info, 
-                          travel %in% max(travel))
+  return(info_list)
 }  
 
 
