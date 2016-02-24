41cb72822b72b2d6d6d5148b121001d077310a21
Chinh-Le <chinhkieule@hotmail.com>
2016-02-24 01:18:18 -0800

    Fixed summary file and paragraph

--- scripts/summary.R
+++ scripts/summary.R
@@ -1,27 +1,47 @@
 # Function that takes in data from the class survey and returns a list of information
 
 library(dplyr)
+data <- read.csv("https://raw.githubusercontent.com/INFO-498F/a7-survey-data/master/intro_survey_data.csv", stringsAsFactors = FALSE)
 
-info_summary <- function(dataset) {
+info_summary <- function(data) {
   
-  # List of information
+  # Renaming column names
+  data <- rename(data, 
+                 class_standing = What.is.your.current.class.standing., 
+                 informatics = Are.you.interested.in.applying.to.the.Informatics.major.,
+                 rprogram = What.is.your.familiarity.with..Using.the.R.programming.language,
+                 program_ex = What.is.your.programming.experience.,
+                 countries = How.many.countries.have.you.visited.in.your.life.
+  )
+  
   info_list <- list()
   
-  # Calculate the number of students in class
-  info_list$num <- nrow(dataset)
+  # Number of students in each class standing
+  freshman <- sum((data$class_standing) == "Freshman")
+  sophomore <- sum((data$class_standing) == "Sophomore")
+  junior <- sum((data$class_standing) == "Junior")
+  senior <- sum((data$class_standing) == "Senior")
   
-  # Calculate number of students in each class standing
-  info_list$numFreshmen <- nrow(filter(dataset, What.is.your.current.class.standing. == "Freshman"))
-  info_list$numSophomore <- nrow(filter(dataset, What.is.your.current.class.standing. == "Sophomore"))
-  info_list$numJunior <- nrow(filter(dataset, What.is.your.current.class.standing. == "Junior"))
-  info_list$numSenior <- nrow(filter(dataset, What.is.your.current.class.standing. == "Senior"))
+  # Number of students applying to the Informatics program
+  intended <- sum((data$informatics) == "Yes")  
   
-  # Calculate the number of Windows and Mac users
-  info_list$numWindows <- nrow(filter(dataset, What.operating.system.do.you.typically.use. == "Windows"))
-  info_list$numMac <- nrow(filter(dataset, What.operating.system.do.you.typically.use. == "Mac"))
-    
+  # Students familiar with programming
+  experience <- sum((data$rprogram) == "Have used it a few times") %>%
+    sum((data$rprogram) == "Intermediate user")
+  
+  no_exp <- sum((data$program_ex) == "Never written code")
+  
+  # Max number of countries visited by student
+  traveled <- filter(data,
+                     countries %in% max(countries)) %>%
+    select(countries)
+  
+  # List of information about dataset
+  info_list <- list(freshman = freshman, sophomore = sophomore, junior = junior, senior = senior,
+                    total_students = sum(freshman, sophomore, junior, senior), applying = intended,
+                    r_experience = experience, no_exp = no_exp, 
+                    most_countries = traveled)
+  
   return(info_list)
-}  
-
-
-
+  
+}
